c5c11666392cd435d7ebd3483ebd5b40aa0afc92
samis <samilsully@gmail.com>
2020-10-01 06:24:58 -0700

    Rearranged middleware

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
